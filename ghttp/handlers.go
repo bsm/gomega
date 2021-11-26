65a3b353cae80ca1c23abc577c547e41bed30472
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2021-11-26 15:28:22 +0000

    Bump to 1.17.0

--- ghttp/handlers.go
+++ ghttp/handlers.go
@@ -6,7 +6,7 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"net/url"
 	"strings"
@@ -115,7 +115,7 @@ func (g GHTTPWithGomega) VerifyHeaderKV(key string, values ...string) http.Handl
 func (g GHTTPWithGomega) VerifyBody(expectedBody []byte) http.HandlerFunc {
 	return CombineHandlers(
 		func(w http.ResponseWriter, req *http.Request) {
-			body, err := ioutil.ReadAll(req.Body)
+			body, err := io.ReadAll(req.Body)
 			req.Body.Close()
 			g.gomega.Expect(err).ShouldNot(HaveOccurred())
 			g.gomega.Expect(body).Should(Equal(expectedBody), "Body Mismatch")
@@ -131,7 +131,7 @@ func (g GHTTPWithGomega) VerifyJSON(expectedJSON string) http.HandlerFunc {
 	return CombineHandlers(
 		g.VerifyMimeType("application/json"),
 		func(w http.ResponseWriter, req *http.Request) {
-			body, err := ioutil.ReadAll(req.Body)
+			body, err := io.ReadAll(req.Body)
 			req.Body.Close()
 			g.gomega.Expect(err).ShouldNot(HaveOccurred())
 			g.gomega.Expect(body).Should(MatchJSON(expectedJSON), "JSON Mismatch")
